7a36dac6cdc77bf451710627a0fcdce8951fb333
Joao <joaoheron@gmail.com>
2022-04-19 17:08:22 -0300

    Created project config

---
+++ project_config.sh
@@ -0,0 +1,10 @@
+source .env
+
+export DBT_PROJECT="dbt_poc"
+export DBT_TYPE="postgres"
+export DBT_HOST="0.0.0.0"
+export DBT_PORT=5432
+export DBT_DATABASE="dbt_poc"
+export DBT_TARGET_SCHEMA="dbt_poc_dev"
+export DBT_USER=$DBT_USER
+export DBT_PASSWORD=$DBT_PASSWORD
